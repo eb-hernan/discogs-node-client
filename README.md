d70f1484b392024ec7b27b1d91de7b1817589b1e
Hernan Cussi <hernan@eventbrite.com>
2018-07-13 11:30:57 -0300

    Added test call to discogs API

--- README.md
+++ README.md
@@ -3,9 +3,15 @@ DiscoGS API client for node JS
 
 ## Installation Instructions
 
-1. Clone this repository and `cd` into it in a terminal window.
+1. Create an account https://www.discogs.com and create a token
 
-2. Execute the following commands:
+2. Add the token in the env variables to be used in the node app `export DISCOGS_TOKEN="YOUR_PRIVATE_TOKEN"`
+
+3. Confirm that has been added correctly `printenv | grep DISCOGS_TOKEN`
+
+4. Clone this repository and `cd` into it in a terminal window.
+
+5. Execute the following commands:
 
 ```shell
 
--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const Discogs = require('disconnect').Client;
+
+const DISCOGS_TOKEN = process.env.DISCOGS_TOKEN;
+
+
+if (!DISCOGS_TOKEN) {
+    throw new Error('DISCOGS_TOKEN is not defined in the ENV variables');
+}
+
+const dis = new Discogs('EventbriteTest/1.2.1', {userToken: DISCOGS_TOKEN});
+
+const db = dis.database();
 
 console.log("Running...");
+
+db.getRelease(176126, (err, data) => {
+    const {resource_url:url} = data.images[0];
+
+    db.getImage(url, (err, data, rateLimit) => {
+        // Data contains the raw binary image data
+        console.log('url', url, rateLimit);
+    });
+});

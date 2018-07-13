88f61f7b026d9d52f264204342b7375bc928b8c3
Hernan Cussi <hernan@eventbrite.com>
2018-07-13 13:27:37 -0300

    Added mongoDB

--- README.md
+++ README.md
@@ -3,19 +3,28 @@ DiscoGS API client for node JS
 
 ## Installation Instructions
 
+Clone this repository and `cd` into it in a terminal window.
+
+1. `npm install`
+
+## Database
+
+1. Install MongoDB `brew install mongodb@3.4`
+
+2. Create the directory where the data will be stored `mkdir -p data/db`
+
+3. Run mongoDB deamon `npm run-script mongo`
+
+4. Install [`Robo3T`](https://robomongo.org/)
+
+5. Connect to localhost:27017
+
+## Node
+
 1. Create an account https://www.discogs.com and create a token
 
 2. Add the token in the env variables to be used in the node app `export DISCOGS_TOKEN="YOUR_PRIVATE_TOKEN"`
 
 3. Confirm that has been added correctly `printenv | grep DISCOGS_TOKEN`
 
-4. Clone this repository and `cd` into it in a terminal window.
-
-5. Execute the following commands:
-
-```shell
-
-npm install
-
-npm start
-```
+4. `npm start`
--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+const _ = require('lodash');
+const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
 const Discogs = require('disconnect').Client;
 
 const DISCOGS_TOKEN = process.env.DISCOGS_TOKEN;
 
+// Connection URL
+const url = 'mongodb://localhost:27017/';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, { useNewUrlParser: true })
+    .then((client) => {
+        console.log('Connected successfully to server');
+
+        mongodb = client.db('discogs');
+
+        // Get the documents collection
+        const collection = mongodb.collection('artists');
+
+        collection.insertOne({
+            id: '1',
+            name: 'Foo Fighters',
+            image_url: 'url'
+        }).then((result) => {
+                console.log('inserted!!!');
+        }).catch((err) => {
+            console.log('insert error');
+        });
+
+        client.close();
+  })
+  .catch((err) => {});
 
 if (!DISCOGS_TOKEN) {
     throw new Error('DISCOGS_TOKEN is not defined in the ENV variables');
@@ -13,11 +42,31 @@ const db = dis.database();
 
 console.log("Running...");
 
+db.search({type: 'artist'})
+    .then(({pagination, results}) => {
+        console.log('pagination', pagination);
+
+        _.forEach(results, ({id, title, cover_image, thumb}) => {
+
+            console.log({
+                id,
+                name: title,
+                imageUrl: cover_image,
+                thumb: thumb,
+            });
+        });
+    });
+
 db.getRelease(176126, (err, data) => {
     const {resource_url:url} = data.images[0];
 
-    db.getImage(url, (err, data, rateLimit) => {
-        // Data contains the raw binary image data
-        console.log('url', url, rateLimit);
-    });
+    db.getImage(url)
+        .then((err, data, rateLimit) => {
+            // Data contains the raw binary image data
+            console.log('url', url);
+            console.log('data', data);
+            console.log('rateLimit', rateLimit);
+        });
 });
+
+
